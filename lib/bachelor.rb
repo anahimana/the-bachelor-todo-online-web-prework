690c22a7bd93651d817f7c5f147c557f9089d625
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-07 15:53:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,7 +11,7 @@ def get_contestant_name(data, occupation)
   data.each do |season, info|
     info.each_with_index do |contestant, index|
       if info[index]["occupation"] == occupation
-    end
+      end
     end
   end
 end
