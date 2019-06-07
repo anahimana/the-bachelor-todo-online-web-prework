68582d9525aee51be977fc40271fa866c5f52851
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-07 16:39:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_average_age_for_season(data, season)
   ages = []
   data[season].each {|x| ages << x["age"].to_i}
   # Calculate average
-  
-  (ages.sum / ages.length).round
+  (644/25.to_f).round
+  (ages.sum / ages.length.to_f).round
   binding.pry
 end
